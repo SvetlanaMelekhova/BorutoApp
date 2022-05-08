d4198f036f14388ae4a586844281d2028a3a823c
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-05-08 13:13:14 +0300

    create hero remote key entity, hero remote key Dao

--- app/src/main/java/com/svetlana/learn/borutoapp/data/local/BorutoDatabase.kt
+++ app/src/main/java/com/svetlana/learn/borutoapp/data/local/BorutoDatabase.kt
@@ -3,10 +3,13 @@ package com.svetlana.learn.borutoapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.svetlana.learn.borutoapp.data.local.dao.HeroDao
+import com.svetlana.learn.borutoapp.data.local.dao.HeroRemoteKeyDao
 import com.svetlana.learn.borutoapp.domain.model.Hero
+import com.svetlana.learn.borutoapp.domain.model.HeroRemoteKey
 
-@Database(entities = [Hero::class], version = 1)
+@Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
 abstract class BorutoDatabase : RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
+    abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
 }
