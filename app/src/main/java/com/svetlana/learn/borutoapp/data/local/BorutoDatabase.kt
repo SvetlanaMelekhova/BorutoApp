6224f6b3345749a02f7fd9520d15c1f274c1f3d1
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-05-08 14:59:56 +0300

    create converter for list of strings

--- app/src/main/java/com/svetlana/learn/borutoapp/data/local/BorutoDatabase.kt
+++ app/src/main/java/com/svetlana/learn/borutoapp/data/local/BorutoDatabase.kt
@@ -2,12 +2,14 @@ package com.svetlana.learn.borutoapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.svetlana.learn.borutoapp.data.local.dao.HeroDao
 import com.svetlana.learn.borutoapp.data.local.dao.HeroRemoteKeyDao
 import com.svetlana.learn.borutoapp.domain.model.Hero
 import com.svetlana.learn.borutoapp.domain.model.HeroRemoteKey
 
 @Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class BorutoDatabase : RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
